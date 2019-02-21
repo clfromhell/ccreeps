6f330b700847c160e216d5a4e91e79197d814a9b
clfromhell <hackermeier@gmail.com>
2019-02-21 15:22:50 +0100

    patch db + usermgmt

--- dbinterface.py
+++ dbinterface.py
@@ -6,26 +6,26 @@ class DBI:
         conn = sqlite3.connect('pcreeps.db')
         self._c = conn.cursor()
 
-    def createuser(self, nick, pwh):
+    def create_user(self, nick, pwh):
         #TODO create new user in db
         pass
 
-    def getuserid(self, nick):
+    def get_user_id(self, nick):
         #TODO return user id
         pass
 
-    def getusermap(self, nick):
+    def get_user_map(self, nick):
         #TODO return map id
         pass
 
-    def changeuserpw(self, nick, pwh, pwhnew):
+    def change_user_pw(self, nick, pwh, pwhnew):
         #TODO password change
         pass
 
-    def checknickavail(self, nick):
+    def check_nick_avail(self, nick):
         #TODO return True if nick is available
         pass
 
-    def checklogoninfo(self, nick, pwh):
+    def check_logon_info(self, nick, pwh):
         #TODO return 0 if pwh = db pwh
         pass
