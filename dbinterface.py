2544111ec4055954db5a2b6b7bfc84e56963c81c
clfromhell <hackermeier@gmail.com>
2019-02-21 15:24:37 +0100

    patch db + usermgmt

--- dbinterface.py
+++ dbinterface.py
@@ -7,25 +7,25 @@ class DBI:
         self._c = conn.cursor()
 
     def create_user(self, nick, pwh):
-        #TODO create new user in db
+        # TODO create new user in db
         pass
 
     def get_user_id(self, nick):
-        #TODO return user id
+        # TODO return user id
         pass
 
     def get_user_map(self, nick):
-        #TODO return map id
+        # TODO return map id
         pass
 
     def change_user_pw(self, nick, pwh, pwhnew):
-        #TODO password change
+        # TODO password change
         pass
 
     def check_nick_avail(self, nick):
-        #TODO return True if nick is available
+        # TODO return True if nick is available
         pass
 
     def check_logon_info(self, nick, pwh):
-        #TODO return 0 if pwh = db pwh
+        # TODO return 0 if pwh = db pwh
         pass
