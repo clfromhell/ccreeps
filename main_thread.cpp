49a117afc5deb66d197415b9006784971e54544c
liquidiert <haberederkorbinian@googlemail.com>
2019-02-21 13:21:30 +0100

    added comments with thoughts about game

--- main_thread.cpp
+++ main_thread.cpp
@@ -3,6 +3,17 @@
 #include <string>
 using namespace std;
 
-int main(int argc, char *argv[]) {
+/*
+ * Start point for ccreeps -> starts class threads and map
+ * I thought of different classes, all handled by their own thread, while one
+ * map handled by one thread but talking to the class threads
+ *
+ * different start modes possible:
+ * - probably start with reduced threads
+ * - etc.
+ *
+ */
+
+int main(int argc, char *argv[]) { //args for future use
     cout << argv[1] << endl;
 }
