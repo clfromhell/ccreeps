cb57f0a40b52eebba74c2a18c29e1272ba657870
liquidiert <haberederkorbinian@googlemail.com>
2019-02-21 19:57:34 +0100

    added database todo: - schau mal wasd doa kannsd :'D

--- worker_class.py
+++ worker_class.py
@@ -1,12 +1,14 @@
 # main class of this file
 class Worker:
 
-    def __init__(self):  # probably hash path
-        self.cmd = None
-        self.status = None
+    def __init__(self, cmd, id):  # probably hash path
+        self._cmd = cmd
+        self._id = id
+        self._status = None
         self.check_for_new_cmd()
 
     def check_for_new_cmd(self):
         import Command
-        if self.cmd != Command.get_cmd():
-            self.cmd = Command.get_cmd()
+
+        if self._cmd != Command.get_cmd():
+            self._cmd = Command.get_cmd()
